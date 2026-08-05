43a460b8d73ec1d511b4ea14c02ef0b6209b2adf
MerlinMomo12 <momofunk@gmx.de>
2026-08-05 16:37:36 +0200

    Another GUI Test

--- src/main/java/de/merlinmomo12/createpowergridinstrumentation/content/transmitter/base/TransmitterScreen.java
+++ src/main/java/de/merlinmomo12/createpowergridinstrumentation/content/transmitter/base/TransmitterScreen.java
@@ -1,204 +1,89 @@
 package de.merlinmomo12.createpowergridinstrumentation.content.transmitter.base;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import de.merlinmomo12.createpowergridinstrumentation.CreatePowergridInstrumentation;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class TransmitterScreen extends AbstractContainerScreen<TransmitterMenu> {
 
-    public TransmitterScreen(
-            TransmitterMenu menu,
-            Inventory inventory,
-            Component title
-    ) {
+    private static final ResourceLocation GUI =
+            ResourceLocation.fromNamespaceAndPath(
+                    CreatePowergridInstrumentation.MODID,
+                    "textures/gui/transmitter.png"
+            );
+
+    public TransmitterScreen(TransmitterMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
 
-        this.imageWidth = 220;
-        this.imageHeight = 180;
+        imageWidth = 176;
+        imageHeight = 166;
+
+        inventoryLabelX = 8;
+        inventoryLabelY = imageHeight - 94;
+
+        titleLabelX = 4;
+        titleLabelY = 3;
     }
 
-
     @Override
-    protected void renderBg(
-            GuiGraphics graphics,
-            float partialTick,
-            int mouseX,
-            int mouseY
-    ) {
-        int left = this.leftPos;
-        int top = this.topPos;
+    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
+        int x = leftPos;
+        int y = topPos;
 
+        RenderSystem.setShaderTexture(0, GUI);
+        graphics.blit(GUI, x, y, 0, 0, imageWidth, imageHeight);
 
-        // Hintergrund
-        graphics.fill(
-                left,
-                top,
-                left + imageWidth,
-                top + imageHeight,
-                0xFF202020
-        );
+        // ===== TYPE =====
+        drawCenteredString(graphics, "Temperature", 4, 3, 167, 11, 0x404040);
 
+        // ===== LOWER RANGE =====
+        drawCenteredString(graphics, "0.00", 105, 28, 123, 41, 0x404040);
 
-        // Kopfbereich
-        graphics.fill(
-                left,
-                top,
-                left + imageWidth,
-                top + 25,
-                0xFF303030
-        );
+        // ===== UPPER RANGE =====
+        drawCenteredString(graphics, "100.00", 105, 50, 123, 63, 0x404040);
 
+        // ===== LOWER UNIT =====
+        drawCenteredString(graphics, "°C", 141, 28, 154, 41, 0x404040);
 
-        // Trennlinien
-        graphics.fill(
-                left + 10,
-                top + 55,
-                left + imageWidth - 10,
-                top + 56,
-                0xFF606060
-        );
+        // ===== UPPER UNIT =====
+        drawCenteredString(graphics, "°C", 141, 50, 154, 63, 0x404040);
 
-        graphics.fill(
-                left + 10,
-                top + 110,
-                left + imageWidth - 10,
-                top + 111,
-                0xFF606060
-        );
-
-
-        // Statusfeld
-        graphics.fill(
-                left + 15,
-                top + 65,
-                left + 205,
-                top + 100,
-                0xFF151515
-        );
-
-
-        // Button Simulation
-        graphics.fill(
-                left + 70,
-                top + 135,
-                left + 150,
-                top + 160,
-                0xFF404040
-        );
-
-
-        // Button Hover Effekt
-        if(mouseX >= left + 70 &&
-                mouseX <= left + 150 &&
-                mouseY >= top + 135 &&
-                mouseY <= top + 160) {
-
-            graphics.fill(
-                    left + 70,
-                    top + 135,
-                    left + 150,
-                    top + 160,
-                    0xFF606060
-            );
-        }
+        // ===== STATUS =====
+        drawCenteredString(graphics, "OK", 4, 80, 139, 103, 0x00AA00);
     }
 
-
     @Override
-    protected void renderLabels(
-            GuiGraphics graphics,
-            int mouseX,
-            int mouseY
-    ) {
-
-        // Titel
-        graphics.drawString(
-                this.font,
-                "Temperature Transmitter",
-                10,
-                8,
-                0xFFFFFF
-        );
-
-
-        // Werte
-        graphics.drawString(
-                this.font,
-                "Input:",
-                15,
-                35,
-                0xAAAAAA
-        );
-
-        graphics.drawString(
-                this.font,
-                "75.0 °C",
-                80,
-                35,
-                0xFFFFFF
-        );
-
-
-        graphics.drawString(
-                this.font,
-                "Output:",
-                15,
-                75,
-                0xAAAAAA
-        );
-
-        graphics.drawString(
-                this.font,
-                "12.4 mA",
-                80,
-                75,
-                0x55FF55
-        );
-
-
-        graphics.drawString(
-                this.font,
-                "HART Range:",
-                15,
-                120,
-                0xAAAAAA
-        );
-
-
-        graphics.drawString(
-                this.font,
-                "0 - 100 °C",
-                100,
-                120,
-                0xFFFFFF
-        );
-
-
-        graphics.drawCenteredString(
-                this.font,
-                "Configure",
-                110,
-                143,
-                0xFFFFFF
-        );
-    }
-
-
-    @Override
-    public void render(
-            GuiGraphics graphics,
-            int mouseX,
-            int mouseY,
-            float partialTick
-    ) {
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        renderBackground(graphics, mouseX, mouseY, partialTick);
         super.render(graphics, mouseX, mouseY, partialTick);
+        renderTooltip(graphics, mouseX, mouseY);
+    }
 
-        // Tooltips etc.
-        this.renderTooltip(
-                graphics,
-                mouseX,
-                mouseY
-        );
+    /**
+     * Zeichnet einen String horizontal und vertikal zentriert innerhalb
+     * der angegebenen Box.
+     */
+    private void drawCenteredString(
+            GuiGraphics graphics,
+            String text,
+            int x1,
+            int y1,
+            int x2,
+            int y2,
+            int color) {
+
+        int boxWidth = x2 - x1;
+        int boxHeight = y2 - y1;
+
+        int textWidth = font.width(text);
+
+        int drawX = leftPos + x1 + (boxWidth - textWidth) / 2;
+        int drawY = topPos + y1 + (boxHeight - font.lineHeight) / 2;
+
+        graphics.drawString(font, text, drawX, drawY, color, false);
     }
 }
